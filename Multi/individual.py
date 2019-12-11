3c7e1820a75584f650fd9f54a37b387b5d364a1e
PatrickTscheng <zhengpf823@gmail.com>
2019-12-11 11:35:37 +0100

    multi sim

---
+++ Multi/individual.py
@@ -0,0 +1,76 @@
+import random
+from buffer import Buffer
+
+
+class Individual:
+
+    def __init__(self, Pi: float, Ri: float,
+                 B_f: Buffer, B_b: Buffer, X_init=False):
+        assert (0 <= Pi and Pi <= 1) is True
+        assert (0 <= Ri and Ri <= 1) is True
+        self.Pi = Pi  # breakdown probability
+        self.Ri = Ri  # repair probability
+        self.x_n = X_init  # state of x false represent down
+        self.flag_b_s = False
+        # True represents the machine is blockage or stravation
+        self.B_f = B_f  # Buffer forward
+        self.B_b = B_b  # Buffer backward
+
+    def __init__(self, Pi: float, Ri: float,
+                 B: Buffer, flag: bool, X_init=False):
+        assert (0 <= Pi and Pi <= 1) is True
+        assert (0 <= Ri and Ri <= 1) is True
+        self.Pi = Pi  # breakdown probability
+        self.Ri = Ri  # repair probability
+        self.x_n = X_init  # state of x false represent down
+        self.flag_b_s = False
+        # True represents the machine is blockage or stravation
+        if flag:  # flag is a special condition control the first(true) and last one(false)
+            self.B_b = B  # Buffer backward
+            self.B_f = None
+        else:
+            self.B_b = None
+            self.B_f = B  # Buffer forward
+
+    # simulation of a time slot
+    # return 1 represent a product return 0 represent no product
+    def run_once(self):  # -> int bool
+
+        ONE_PRODUCT = 1
+        NO_PRODUCT = 0
+
+        if self.x_n is False:
+            if random.random() <= self.Ri:
+                self.x_n = True
+        else:
+            if random.random() <= self.Pi:
+                self.x_n = False
+
+        if (self.x_n and (not self.flag_b_s)):
+            self.buffer_change()
+            return ONE_PRODUCT, self.x_n
+        else:
+            return NO_PRODUCT, self.x_n
+
+    def blockage(self, B: Buffer):
+        self.flag_b_s is B.check_full()
+
+    def starvation(self, B: Buffer):
+        self.flag_b_s is B.check_null()
+
+    def b_and_s(self):
+        if self.B_f is None:
+            self.blockage(self.B_b)
+        elif self.B_b is None:
+            self.starvation(self.B_f)
+        else:
+            self.starvation(self.B_f) or self.blockage(self.B_b)
+
+    def buffer_change(self):
+        if self.B_f is None:
+            self.B_b.add_one()
+        elif self.B_b is None:
+            self.B_f.take_one()
+        else:
+            self.B_b.add_one()
+            self.B_f.take_one()
---
+++ Multi/multi_machine.py
@@ -0,0 +1,49 @@
+from individual import Individual
+from buffer import Buffer
+
+
+class MultiMachine:
+    
+    MachineNumber = 4
+
+    def __init__(self, Pi, Ri, N):
+        self.BufferArray = []
+        for i in range(self.MachineNumber-1): self.BufferArray.append(Buffer(N))
+        # self.B2 = Buffer(N)
+        # self.B3 = Buffer(N)
+
+        self.Machine = []
+        self.Machine.append(Individual(Pi, Ri, self.BufferArray[0], True))
+        # self.M1 = Individual(Pi, Ri, self.B1, True)
+        for i in range(self.MachineNumber-2):
+            self.Machine.append(Individual(Pi, Ri, self.BufferArray[i], self.BufferArray[i+1]))
+        # self.M2 = Individual(Pi, Ri, self.B1, self.B2)
+        # self.M3 = Individual(Pi, Ri, self.B2, self.B3)
+        self.Machine.append(Individual(Pi, Ri, self.BufferArray[2], False))
+
+    def one_slot(self):
+        # step 1: judge if runable
+        for i in range(self.MachineNumber): self.Machine[i].b_and_s()
+        # step 2: begin run
+        M = [0 for i in range(self.MachineNumber)]
+        CR, M[0] = self.Machine[0].run_once()
+        for i in range(self.MachineNumber-2):
+            M[i+1] = self.Machine[i+1].run_once()[1]
+        PR, M[3] = self.Machine[self.MachineNumber-1].run_once()
+        WIP = []
+        ST = []
+        BL = []
+        for i in range(self.MachineNumber-1): WIP.append(self.BufferArray[i].get_measure())
+
+        for i in range(self.MachineNumber-1):
+            if M[i] and self.BufferArray[i].check_null() is True:
+                ST.append(1)
+            else:
+                ST.append(0)
+
+            if M[i] and (not M[i+1]) and self.BufferArray[i].check_full() is True:
+                BL.append(1)
+            else:
+                BL.append(0)
+
+        return PR, CR, WIP, ST, BL
